a656fb6a509e5badc0b97ce4d8b84f6658b58169
Bruno <bruno.oks1@gmail.com>
2022-06-22 19:54:35 -0300

    - adicionado router

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,6 @@ import './bootstrap';
 
 import App from './components/App';
 import {createApp} from "vue";
+import router from './router/index';
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,41 @@
 <template>
-    <h1>App vue</h1>
+    <div class="app">
+        <MainMenu class="menu-area"></MainMenu>
+        <div class="content">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
 <script>
+import MainMenu from './menu/MainMenu';
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        MainMenu
+    }
 }
 </script>
 
 <style scoped>
+.app {
+    margin: 0px;
+    display: grid;
+    grid-template-columns: 15% 1fr;
+    grid-template-rows: 30%;
+    grid-template-areas: "menu content";
+    height: 350vh;
+    background-color: #F5F5F5;
+}
+
+.content {
+    grid-area: content;
+    margin-left: 10px;
+}
+
+.menu-area {
+    grid-area: menu;
+    margin: -10px 10px 0px -10px;
+}
 
 </style>
